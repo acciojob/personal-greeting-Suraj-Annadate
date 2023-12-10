d6b6635238bf0ef8e1ef6c27a8367446c49d1bfd
suraj0698 <annadatesuraj@gmail.com>
2023-12-10 18:43:29 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,20 @@
 
-import React from "react";
+import React, {useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [text, setText] = useState("")
+  function handle(e){
+    setText("Hello "+e.target.value+"!");
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+        <label htmlFor="name">Enter your name:</label><br></br>
+        <input type="text" id="name" onChange={handle}></input>
+        <p>{text}</p>
     </div>
   )
 }
 
-export default App
+export default App;
